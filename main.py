a974e927acc2ce9afdd81b26f2cdd91b6a679764
Gonzalo Gamez <88616733+GamezISR@users.noreply.github.com>
2021-12-13 19:55:00 +0000

    question 3 and 4 added

--- main.py
+++ main.py
@@ -35,3 +35,42 @@ for i in range(1000):
 print("This is how many positives entered: ", positive)
 print("This is how many zeros entered: ", zero)
 
+"""
+3
+
+Make an algorithm to input numbers, reject any numbers that are negative and count many numbers that are positive. When the number zero is input, the process ends and the count of positive numbers is output.
+"""
+counter = 0
+while True:
+  enter = int(input("Enter a number"))
+  if enter < 0:
+    print("Enter a non negative number.")
+  elif enter > 0:
+    counter = counter + 1
+  else:
+    break
+
+print("This is how many positive numbers were entered: ", counter)
+
+
+"""
+4
+
+Write an algorithm to input three different numbers, and then output the largest number.
+"""
+
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+number_3 = int(input("Enter third number: "))
+
+if number1 > number2 and number1 > number3:
+  print(number_1, " is the largest number")
+elif number2 > number1 and number2 > number3:
+  print(number_2, " is the largest number")
+else:
+  print(number_3, " is the largest number")
+
+
+
+
+
