c78c3d215161f068f9b2ae35e3ab5e6e99b4e65f
Gonzalo Gamez <88616733+GamezISR@users.noreply.github.com>
2021-12-13 19:39:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+1
+
+Write an algorithm to input three different numbers, multiply the two larger numbers together and output the result. Use the variable: Number1, Number2, and Number3 for your numbers and Answer for your result
+"""
+
+number1 = 
+
+
+
+
+
+
+
+
